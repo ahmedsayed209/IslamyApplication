634acca8a0fe45b0012512001b81f4769ff78518
Ahmed Sayed Mohamed <ahmed.sayed.ashourr@gmail.com>
2024-07-31 03:26:36 +0300

    *Init Localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:islamyapplication/core/application_theme_manager.dart';
 import 'package:islamyapplication/layout/layout_view.dart';
 import 'package:islamyapplication/modules/Hadeith_view/hadith_details.dart';
@@ -20,6 +22,9 @@ class MyApplicationIsalami extends StatelessWidget
     return MaterialApp(
       title: "IslamiApp",
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      locale: Locale("en"),
+      supportedLocales: AppLocalizations.supportedLocales,
       theme: ApplicationThemeManager.lightThemeData,
       initialRoute: "/",
       routes: {
